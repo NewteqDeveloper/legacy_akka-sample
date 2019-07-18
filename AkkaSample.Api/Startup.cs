d917e075d3efb522308dba589258829b67b7efc0
Newteq <newt.fourie@entelect.co.za>
2019-07-18 18:17:29 +0200

    refactor so that I can read easier which does what and how it's done

--- AkkaSample.Api/Startup.cs
+++ AkkaSample.Api/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Akka.Actor;
+﻿using Akka.Actor;
 using Akka.Configuration;
 using Akka.Routing;
 using AkkaSample.Api.Actors;
@@ -12,13 +8,14 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace AkkaSample.Api
 {
     public class Startup
     {
+        private const string actorSystemName = "akka-sample-api";
+        private const string echoActorName = "echo";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -29,28 +26,9 @@ namespace AkkaSample.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
-            var config = ConfigurationFactory.FromObject(new
-            {
-                akka = Configuration.GetSection("Akka").Get<AkkaConfig>()
-            });
-            var akkaConfig = ConfigurationLoader.Load();
-
-            services.AddSingleton(x => ActorSystem.Create("my-system", config));
-            // services.AddSingleton(x => ActorSystem.Create("my-system", akkaConfig));
-            // services.AddSingleton(x => ActorSystem.Create("my-system"));
-            services.AddSingleton<EchoActorProvider>(provider =>
-            {
-                var actorSystem = provider.GetService<ActorSystem>();
-                // var echoActor = actorSystem.ActorOf<EchoActor>("echo");
-                //var echoActor = actorSystem.ActorOf(Props.Create<EchoActor>()
-                //    .WithRouter(new RoundRobinPool(5)), "echo");
-                var echoActor = actorSystem.ActorOf(Props.Create<EchoActor>().WithRouter(FromConfig.Instance), "echo");
-                return () => echoActor;
-            });
+            CreateActorsWithAppSettings(services);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime lifetime)
         {
             lifetime.ApplicationStarted.Register(() =>
@@ -68,6 +46,56 @@ namespace AkkaSample.Api
             }
 
             app.UseMvc();
+        }
+
+        private void CreateActorsWithoutConfig(IServiceCollection services, bool includeCodeConfig = false)
+        {
+            services.AddSingleton(x => ActorSystem.Create(actorSystemName));
+            services.AddSingleton<EchoActorProvider>(provider =>
+            {
+                var actorSystem = provider.GetService<ActorSystem>();
+
+                IActorRef echoActor;
+                if (includeCodeConfig)
+                {
+                    echoActor = actorSystem.ActorOf(Props.Create<EchoActor>()
+                        .WithRouter(new RoundRobinPool(5)), echoActorName);
+                }
+                else
+                {
+                    echoActor = actorSystem.ActorOf<EchoActor>(echoActorName);
+                }
+                return () => echoActor;
+            });
+        }
+
+        private void CreateActorsWithAkkaConf(IServiceCollection services)
+        {
+            var akkaConfig = ConfigurationLoader.Load();
+
+            services.AddSingleton(x => ActorSystem.Create(actorSystemName, akkaConfig));
+            services.AddSingleton<EchoActorProvider>(provider =>
+            {
+                var actorSystem = provider.GetService<ActorSystem>();
+                var echoActor = actorSystem.ActorOf(Props.Create<EchoActor>().WithRouter(FromConfig.Instance), echoActorName);
+                return () => echoActor;
+            });
+        }
+
+        private void CreateActorsWithAppSettings(IServiceCollection services)
+        {
+            var config = ConfigurationFactory.FromObject(new
+            {
+                akka = Configuration.GetSection("Akka").Get<AkkaConfig>()
+            });
+
+            services.AddSingleton(x => ActorSystem.Create(actorSystemName, config));
+            services.AddSingleton<EchoActorProvider>(provider =>
+            {
+                var actorSystem = provider.GetService<ActorSystem>();
+                var echoActor = actorSystem.ActorOf(Props.Create<EchoActor>().WithRouter(FromConfig.Instance), echoActorName);
+                return () => echoActor;
+            });
         }
     }
 }
