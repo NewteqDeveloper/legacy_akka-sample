bd40f3270d8ab41dc0635d1df4beaf069ee51983
Newteq <newt.fourie@entelect.co.za>
2019-07-18 11:05:54 +0200

    Initial Commit

---
+++ AkkaSample/Actors/EchoActor.cs
@@ -0,0 +1,32 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AkkaSample.Actors
+{
+    public class EchoActor : UntypedActor
+    {
+        protected override void PreStart()
+        {
+            base.PreStart();
+            WriteMessage($"In PreStart");
+        }
+
+        protected override void PostStop()
+        {
+            base.PostStop();
+            WriteMessage($"In PostStop");
+        }
+
+        protected override void OnReceive(object message)
+        {
+            WriteMessage($"Message has been received :: {message}");
+        }
+
+        private void WriteMessage(string message)
+        {
+            Console.WriteLine($"Action Message from {nameof(EchoActor)}. This message is:\r\n--{message}");
+        }
+    }
+}
---
+++ AkkaSample/Program.cs
@@ -0,0 +1,18 @@
+﻿using Akka.Actor;
+using AkkaSample.Actors;
+using System;
+
+namespace AkkaSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var actorSystem = ActorSystem.Create("akka-sample");
+            var echo = actorSystem.ActorOf<EchoActor>(nameof(EchoActor));
+            echo.Tell("Hello World");
+
+            Console.ReadLine();
+        }
+    }
+}
